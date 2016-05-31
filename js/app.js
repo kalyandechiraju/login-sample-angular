a335f51dde1c355e2ef59f1f0d28163bc1a0e1c0
kalyandechiraju <kalyandechiraju@outlook.com>
2016-06-01 01:05:12 +0530

    Sample login form with dummy validations

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-var app = angular.module('loginapp', [ ]);
+(function() {
+    var app = angular.module('loginapp', []);
+
+    app.controller('LoginController', function($scope) {
+        $scope.isLoggedIn = false;
+        //this.username = email.
+        $scope.loginUser = function() {
+            $scope.isLoggedIn = true;
+            $scope.username = $scope.email;
+        };
+
+        $scope.logoutUser = function() {
+            $scope.isLoggedIn = false;
+            $scope.email = null;
+            $scope.password = null;
+        };
+    });
+})();
